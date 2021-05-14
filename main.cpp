7afc6db9420db2b37b388fa37332cba788733390
FranklinNavas <>
2021-05-14 01:00:26 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int numero1;
+int numero2;
+
+bool booleana=true;
+int main()
+{
+  cout<<"Ingrese la edad de la perosna 1:"<<endl;
+  cin>>numero1;
+  cout<<"Ingrese la edad de la perosna 2:"<<endl;
+  cin>>numero2;
+  if(numero1==numero2)
+  {
+    cout<<"Verdadero"<<endl;
+  }
+  else
+  {
+  cout<<"Falso"<<endl;
+  }
+}
